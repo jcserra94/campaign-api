341e10051ddee75783fd60bfb363b079d2a0826d
JC <jc_serrano1994@yahoo.com>
2026-04-22 19:00:10 -0700

    Add .gitignore and remove pycache from tracking

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from datetime import datetime, timezone
-from typing import Annotated, Any, Generic, TypeVar
-from fastapi import Depends, FastAPI, HTTPException, Request
+from typing import Annotated, Any, Generic, Optional, TypeVar
+from fastapi import Depends, FastAPI, HTTPException, Query, Request
 from fastapi.concurrency import asynccontextmanager
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel, Session, create_engine, select
+from sqlmodel import Field, SQLModel, Session, create_engine, func, select
 
 class Campaign(SQLModel, table=True):
     campaign_id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     due_date: datetime | None = Field(default=None, index=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=True, index=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), index=True)
 
 class CampaignCreate(SQLModel):
     name: str
@@ -34,8 +34,8 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     with Session(engine) as session:
         if not session.exec(select(Campaign)).first():
-            session.add_all([Campaign(name = "Sosa Rise", due_date = datetime.now()),
-                             Campaign(name = "Sosa Takeover", due_date = datetime.now())])
+            session.add_all([Campaign(name = "Sosa Rise", due_date = datetime.now(timezone.utc)),
+                             Campaign(name = "Sosa Takeover", due_date = datetime.now(timezone.utc))])
             session.commit()
     yield
     
@@ -56,11 +56,42 @@ T = TypeVar("T")
 class Response(BaseModel, Generic[T]):
     data: T
 
+#@app.get("/campaigns", response_model=Response[list[Campaign]])
+#async def read_campaigns(session: SessionDep, page: int = Query(1, ge=1)):
+#    print(f"Page: {page}")
+#    limit = 20
+#    offset = (page - 1) * limit
+#    data = session.exec(select(Campaign))(Campaign.campaign_id).offset(offset).limit(limit).all() 
+#    return {"data": data}
 
-@app.get("/campaigns", response_model=Response[list])
-async def read_campaigns(session: SessionDep):
-    data = session.exec(select(Campaign)).all()
-    return {"data": data}
+class PaginationResponse(BaseModel, Generic[T]):
+    data: T
+    next: Optional[str]
+    prev: Optional[str]
+    
+
+@app.get("/campaigns", response_model=PaginationResponse[list[Campaign]])
+async def read_campaigns(request: Request, session: SessionDep, offset: int = Query(0, ge=0),
+                            limit: int = Query(20, ge=1)):
+    
+    # Build the query inside the select()
+    query = select(Campaign).order_by(Campaign.campaign_id).offset(offset).limit(limit)
+    data = session.exec(query).all()
+    
+    base_url = str(request.url).split("?")[0]
+
+    next_url = f"{base_url}?offset={offset + limit}&page_size={limit}" 
+
+    if offset > 0:
+        prev_url = f"{base_url}?offset={max(0, offset - limit)}&page_size={limit}"
+    else:
+        prev_url = None
+
+    return {"data": data,
+            "next": next_url, 
+            "prev": prev_url
+            
+            }
 
 #@app.get("/campaigns")
 #async def read_campaigns():
@@ -152,4 +183,3 @@ async def delete_campaign(id: int, session: SessionDep):
 #            data.pop(index)
 #            return Response(status_code=204)
 #    raise HTTPException(status_code=404, detail="Campaign not found")
-
